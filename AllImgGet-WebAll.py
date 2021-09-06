5c64ee04a4161b02985abcde2953dffb889351d9
JackAugust <JackAugustZJ@gmail.com>
2021-09-06 09:21:53 +0800

    Realize the retransmission mechanism due to network problems, the current version can be V1.0, and the general functions have been implemented

--- AllImgGet-WebAll.py
+++ AllImgGet-WebAll.py
@@ -1,6 +1,7 @@
 import os
 import time
-
+import urllib3
+urllib3.disable_warnings()
 import socket
 
 dir = r"D:/Let'sFunning/Picture/PythonGet/"
@@ -10,17 +11,27 @@ url = "https://pic.xrmn5.com"
 # 链接：
 # https://blog.csdn.net/IllegalName/article/details/77164521?depth_1-utm_source=distribute.pc_relevant.none-task&utm_source=distribute.pc_relevant.none-task
 
-socket.setdefaulttimeout(20)
+# 设置超时时间
+time_out = 60
+socket.setdefaulttimeout(20*time_out)
 
 import requests
 
 headers = {
-'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36 Edg/92.0.902.84'
+'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36 Edg/92.0.902.84',
+	'Connection':'close'
 }
 
 URL = "https://www.xrmn5.com/XiuRen/"
 WebURL = "https://www.xrmn5.com/"
-Get_url = requests.get(URL,headers=headers)
+
+time.sleep(1)
+
+# 去除添加ssh无认证下的红色警告
+# https://urllib3.readthedocs.io/en/1.26.x/advanced-usage.html#ssl-warnings
+requests.packages.urllib3.disable_warnings()
+
+Get_url = requests.get(URL,headers=headers,timeout=time_out,verify=False)
 Get_url.encoding = 'utf-8'
 Get_html = Get_url.text
 # 关闭当前链接
@@ -52,37 +63,63 @@ for pagenum in range(int(PageNum)):
 	print(inforName)
 	print(likeNum)
 	num = len(likeNum)
-	
+
 	patren3 = '<img onload=.*? alt=.*? title=.*? src=\"(.*?)\" />'
-	
+
 	for i in range(num):
 		if (int(likeNum[i][1]) > 500):
 			getImgDir = dir + str(inforName[i][0]) + '/' + str(likeNum[i][0]) + '/' + str(inforName[i][1] + '/')
+			file_num = "".join(list(filter(str.isdigit, getImgDir)))
+
 			# 创建对应目录
 			if not os.path.exists(getImgDir):
 				os.makedirs(getImgDir)
 			else:
 				print("此目录已存在：",getImgDir)
-
+				if (len(os.listdir(getImgDir)) >= (int(file_num[12:]))):
+					continue
 			imgUrl = url + urls[i][2]
 			imgName = getImgDir + urls[i][2].split('/')[-1]
 			print(imgUrl,imgName)
-			# time.sleep(1)
+
 			# 获取封面图片
 			if os.path.isfile(imgName):
 				print("此封面已存在：", imgName)
 			else:
-				Get_Img = requests.get(imgUrl, headers=headers)
-				with open(imgName, 'wb') as f:
-					f.write(Get_Img.content)
-				Get_Img.close()
+				time.sleep(1)
+				try:
+					requests.packages.urllib3.disable_warnings()
+					Get_Img = requests.get(imgUrl, headers=headers,timeout=time_out,verify=False)
+					with open(imgName, 'wb') as f:
+						f.write(Get_Img.content)
+					Get_Img.close()
+				except Exception as e:
+					print('get the first img with the error:  %s ' % e)
+					time.sleep(1)
+					requests.packages.urllib3.disable_warnings()
+					Get_Img = requests.get(imgUrl, headers=headers, timeout=time_out, verify=False)
+					with open(imgName, 'wb') as f:
+						f.write(Get_Img.content)
+					Get_Img.close()
 			# 进入具体网页
 			IntoPageUrl = WebURL + urls[i][0]
 			print("当前写真网页为：",IntoPageUrl)
-			Get_InPage = requests.get(IntoPageUrl, headers=headers)
-			Get_InPage.encoding = 'utf-8'
-			Get_InPagehtml = Get_InPage.text
-			Get_InPage.close()
+			time.sleep(1)
+			try:
+				requests.packages.urllib3.disable_warnings()
+				Get_InPage = requests.get(IntoPageUrl, headers=headers,timeout=time_out,verify=False)
+				Get_InPage.encoding = 'utf-8'
+				Get_InPagehtml = Get_InPage.text
+				Get_InPage.close()
+			except Exception as e:
+				print('get the img page with the error:  %s ' % e)
+				time.sleep(1)
+				requests.packages.urllib3.disable_warnings()
+				Get_InPage = requests.get(IntoPageUrl, headers=headers, timeout=time_out, verify=False)
+				Get_InPage.encoding = 'utf-8'
+				Get_InPagehtml = Get_InPage.text
+				Get_InPage.close()
+
 			AllPage = re.findall('</a><a href=\"(.*?)\">([0-9]*)', Get_InPagehtml)
 
 			for k in range(len(AllPage)):
@@ -94,17 +131,28 @@ for pagenum in range(int(PageNum)):
 					print(GetPageImg)
 					PageImgeName = getImgDir + imgPageUrl[l].split('/')[-1]
 					print(PageImgeName)
-					# time.sleep(1)
+
 					# 获取内部图片
 					if os.path.isfile(PageImgeName):
 						print("此图片已存在：",PageImgeName)
+						continue
 
 					else:
-						Get_PImg = requests.get(GetPageImg, headers=headers)
-						with open(PageImgeName, 'wb') as f:
-							f.write(Get_PImg.content)
-						Get_PImg.close()
-
+						try:
+							time.sleep(1)
+							requests.packages.urllib3.disable_warnings()
+							Get_PImg = requests.get(GetPageImg, headers=headers,timeout=time_out,verify=False)
+							with open(PageImgeName, 'wb') as f:
+								f.write(Get_PImg.content)
+							Get_PImg.close()
+						except Exception as e:
+							time.sleep(1)
+							requests.packages.urllib3.disable_warnings()
+							Get_PImg = requests.get(GetPageImg, headers=headers, timeout=time_out,verify=False)
+							print('get the next img with the error:  %s ' % e)
+							with open(PageImgeName, 'wb') as f:
+								f.write(Get_PImg.content)
+							Get_PImg.close()
 				if k == len(AllPage) - 1:
 					print("当前信息：",AllPage[k])
 					continue
@@ -112,13 +160,24 @@ for pagenum in range(int(PageNum)):
 				# 继续下一页获取图片
 				NewPaperUrl = WebURL + AllPage[k][0]
 				print("开始下一页：",NewPaperUrl)
-				# time.sleep(1)
-				Get_InPage = requests.get(NewPaperUrl, headers=headers)
+				time.sleep(1)
+				requests.packages.urllib3.disable_warnings()
+				Get_InPage = requests.get(NewPaperUrl, headers=headers,timeout=time_out)
 				Get_InPage.encoding = 'utf-8'
 				Get_InPagehtml = Get_InPage.text
 				Get_InPage.close()
 	print("开始下一轮：",GetAllPage[pagenum])
-	Get_url = requests.get(GetAllPage[pagenum],headers=headers)
-	Get_url.encoding = 'utf-8'
-	Get_html = Get_url.text
-	Get_url.close()
+	try:
+		time.sleep(1)
+		requests.packages.urllib3.disable_warnings()
+		Get_url = requests.get(GetAllPage[pagenum],headers=headers,timeout=time_out)
+		Get_url.encoding = 'utf-8'
+		Get_html = Get_url.text
+		Get_url.close()
+	except Exception as e:
+		print('get the next info page with the error:  %s ' % e)
+		requests.packages.urllib3.disable_warnings()
+		Get_url = requests.get(GetAllPage[pagenum],headers=headers,timeout=time_out)
+		Get_url.encoding = 'utf-8'
+		Get_html = Get_url.text
+		Get_url.close()
